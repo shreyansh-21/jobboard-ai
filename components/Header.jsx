3b797930c5f2e4417407672c467b9dd94288069b
Shreyansh Dubey <shreyansh2128@gmail.com>
2025-07-10 16:27:57 +0530

    feat: Update Header component to include user check and adjust Prisma import path Checkpoint

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,10 +5,10 @@ import Link from "next/link";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import {DropdownMenu,DropdownMenuContent,DropdownMenuItem,DropdownMenuTrigger,} from "@/components/ui/dropdown-menu";
 import Image from "next/image";
-// import { checkUser } from "@/lib/checkUser";
+import { checkUser } from "@/lib/checkUser";
 
 export default async function Header() {
-//   await checkUser();
+  await checkUser();
 
   return (
     <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
