4eb6e45b419b4f7e760114a6c8d4db6df5e05e2f
Shreyansh Dubey <shreyansh2128@gmail.com>
2025-07-12 18:06:25 +0530

    feat: Update onboarding pages to check user onboarding status and redirect accordingly

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { getUserOnboardingStatus } from '@/actions/user';
+import { redirect } from "next/navigation";
 
-const IndustyInsightPage = () => {
+const IndustyInsightPage = async () => {
+    const {isOnboarded} = await getUserOnboardingStatus();
+    if (!isOnboarded) {
+      redirect('/onboarding');}
   return (
     <div>
-      
+      OnboardingForm
     </div>
   )
 }
