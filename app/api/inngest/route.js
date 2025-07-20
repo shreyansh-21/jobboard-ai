d4d809462095e189113cb562b16f5f07a8e0b24b
Shreyansh Dubey <shreyansh2128@gmail.com>
2025-07-20 13:27:12 +0530

    feat:Setup a Cron job and  Implement generateIndustryInsights function and update Inngest client configuration

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,5 +1,6 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest/client";
+import { generateIndustryInsights } from "@/lib/inngest/functions";
 
 
 // Create an API that serves zero functions
@@ -7,5 +8,6 @@ export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     /* your functions will be passed here later! */
+    generateIndustryInsights
   ],
 });
